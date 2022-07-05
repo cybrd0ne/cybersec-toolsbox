4475ffceb75b515e780de775964513c381f2c3bc
Dan Candea <Dan.Candea@emea.nttdata.com>
2022-07-05 14:33:04 +0000

    Added GetEvtx.ps1

---
+++ forensics/windows/GetEvtx.ps1
@@ -0,0 +1,8 @@
+# Config
+$logFileName = "Security" # Add Name of the Logfile (System, Application, etc)
+$path = "C:\nttir\" # Add Path, needs to end with a backsplash
+
+# do not edit
+$exportFileName = $logFileName + (get-date -f yyyyMMdd) + ".evt"
+$logFile = Get-WmiObject Win32_NTEventlogFile | Where-Object {$_.logfilename -eq $logFileName}
+$logFile.backupeventlog($path + $exportFileName)
